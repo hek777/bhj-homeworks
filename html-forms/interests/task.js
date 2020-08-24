0fec0d006c86b4537b9f5bfc20f459a5eb59cb3d
Nikita <60575903+hek777@users.noreply.github.com>
2020-08-24 23:15:04 +0300

    Update task.js

---
+++ html-forms/interests/task.js
@@ -0,0 +1,29 @@
+
+let menuItems = document.querySelectorAll(".interest__check");
+let firstMenuItem = menuItems.item(0);
+let fourthMenuItem = menuItems.item(3);
+
+firstMenuItem.addEventListener("click", function() {
+    if (firstMenuItem.checked) {
+        menuItems.item(1).checked = true;
+        menuItems.item(2).checked = true;
+
+    }else {
+        menuItems.item(1).checked = false;
+        menuItems.item(2).checked = false;
+        firstMenuItem.indeterminate = true;
+
+    }
+    
+})
+fourthMenuItem.addEventListener("click", function() {
+    if (fourthMenuItem.checked) {
+        menuItems.item(4).checked = true;
+        menuItems.item(5).checked = true;
+    }else {
+        menuItems.item(4).checked = false;
+        menuItems.item(5).checked = false;
+        fourthMenuItem.indeterminate = true;
+
+    }
+})
