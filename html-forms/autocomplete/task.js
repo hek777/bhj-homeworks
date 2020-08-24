211fad316332388c030e31e2f049b23cc7610231
Nikita <60575903+hek777@users.noreply.github.com>
2020-08-24 23:13:19 +0300

    Update task.js

--- html-forms/autocomplete/task.js
+++ html-forms/autocomplete/task.js
@@ -68,6 +68,8 @@ class Autocomplete {
   }
 
   getMatches( text ) {
+    
+    
     /*
       TODO: этот метод нужно дописать
       text - фраза, которую вводят в поле поиска
@@ -81,12 +83,22 @@ class Autocomplete {
         value: 'Содержимое атрибута value'
       }
     */
-    return [
-      {
-        text: 'Чубакка',
-        value: '1'
+    // return [
+    //   {
+    //     text: 'Чубакка',
+    //     value: '1'
+    //   }
+    // ];
+    let textContainer = [];
+    for (let i = 0; i < this.input.options.length; i++) {
+      if (this.input.options[i].textContent.includes(text) === true) {
+        textContainer.push({
+          text: this.input.options[i].textContent,
+          value: this.input.options[i].value
+        })
       }
-    ];
+    }
+    return textContainer;
   }
 }
 
